787d7c8c6d071594bfd4d9846a8db59492965bc5
mukeshrajbanshi <109079929+mukeshrajbanshi@users.noreply.github.com>
2022-09-23 14:41:34 +0530

    Add files via upload

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="score-board">
+      <h2>Score : {score}</h2>
+    </div>
+  );
+};
+
+export default ScoreBoard;
